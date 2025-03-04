1cfa9e91e257584a8180f6709478f95a85919c29
falcon911-dev <53480170+falcon911-dev@users.noreply.github.com>
2025-03-04 10:52:41 +0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Пример скрипта для плавного скроллинга при клике на ссылки
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
